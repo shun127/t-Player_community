4b93cfd326693138f720ec2ee8b9490a11d297e0
shun127 <cielo720136@gmail.com>
2020-08-01 22:47:44 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # has_secure_passwordメソッドを追加してください
   has_secure_password
   
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
-  # 以下のバリデーションを削除してください
   
   def posts
     return Post.where(user_id: self.id)
