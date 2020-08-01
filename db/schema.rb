77309beb7d51d4f0c74d6336b8f0df4a673c8877
shun127 <cielo720136@gmail.com>
2020-08-01 14:40:22 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_092832) do
+ActiveRecord::Schema.define(version: 2020_08_01_052430) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -23,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_07_31_092832) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
+    t.string "password"
   end
 
 end
